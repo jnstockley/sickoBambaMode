a0693f34a6d88738221a89f75872646a4e85767a
masoncodes <dubstepper016@gmail.com>
2019-01-29 21:33:02 -0600

    added html, css, js, readme, sound files

--- myJs.js
+++ myJs.js
@@ -1,1 +1,31 @@
+/* so i got pause/play working so now we just need 'on the fly' volume changing */
 
+function playSicko() {
+    var sickomode = document.getElementById("sicko");
+    sickomode.play();
+}
+
+function pauseSicko() {
+    var sickomode = document.getElementById("sicko");
+    sickomode.pause();
+}
+
+function playBamba() {
+    var mobamba = document.getElementById("bamba");
+    mobamba.play();
+}
+
+function pauseBamba() {
+    var mobamba = document.getElementById("bamba");
+    mobamba.pause();
+}
+
+function sweetJesus() {
+    playSicko();
+    playBamba();
+}
+
+function pauseBoth() {
+    pauseSicko();
+    pauseBamba();
+}
